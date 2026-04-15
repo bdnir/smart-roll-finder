a60e299567c77ae235c92f414d5ac32c9e9b3242
bdnir <badichi.nir@gmail.com>
2026-04-15 18:30:03 +0300

    Update from 111 to 1 tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -77,7 +77,7 @@ export default {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "slow-glow": "slow-glow 111s ease-in-out infinite",
+        "slow-glow": "slow-glow 1s ease-in-out infinite",
         "slide-up": "slide-up 0.3s ease-out",
       },
     },
